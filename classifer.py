e06cdcece1e0d42413e282efd8cbc31810ea9973
jakrun <jakob.rundlett@gmail.com>
2026-04-18 12:30:13 +0200

    emotion guessing game for classifier

--- classifer.py
+++ classifer.py
@@ -2,46 +2,21 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import random
 import torch
 import os
 import math
 from fer2013 import EmotionCNN
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-batch_size = 32
-
-transform = transforms.Compose([
-    transforms.Grayscale(),          # ensure 1 channel
-    transforms.Resize((48, 48)),     # FER standard size
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
-val_dataset = datasets.ImageFolder("1/test", transform=transform)
-val_loader = DataLoader(val_dataset,  batch_size=batch_size)
-
-path_to_model = "models/" + os.listdir("models")[-1]
-print(path_to_model)
-model = EmotionCNN()
-model.load_state_dict(torch.load(path_to_model, map_location=device))
-model.to(device)
-model.eval()
-
-all_preds    = []
-all_percents = []
-all_classes  = []
-all_targets  = []
-with torch.no_grad():
-    for images, labels in val_loader:
-        images = images.to(device)
-        logits = model(images)              # shape (N, C)
-        probs = torch.softmax(logits, dim=1)
-        preds = torch.argmax(logits, dim=1) # predicted class indices
-        percents = probs[ torch.arange(probs.size(0)), labels.to(device) ] # similar to preds, but instead of class indicies, it is percents of the target class
-        all_preds.append(preds.cpu())
-        all_percents.append(percents.cpu())
-        all_classes.append(probs.cpu())
-        all_targets.append(labels.cpu())
+emotions = [
+    'ange',
+    'disg',
+    'fear',
+    'happ',
+    'neut',
+    'sadn',
+    'surp'
+    ]
 
 def get_data_distribution(directory):
     emotion_dist = []
@@ -72,18 +47,102 @@ def confusion_matrix_percent(perc, targets, num_classes):
             cm[t, c] += p[c]/test_dist[t]
     return cm
 
+def precision_recall(all_preds, all_targets):
+    accuracy = 0
+    total_samples = 0
+    # TP, FP, FN
+    class_accuracy = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    for t, p in zip(all_targets, all_preds):
+        if t == p:
+            accuracy += 1
+            class_accuracy[t][0] += 1
+        else:
+            class_accuracy[p][1] += 1
+            class_accuracy[t][2] += 1
+        total_samples += 1
+    print(f'total accuracy: {(accuracy/total_samples):.2f}')
+    print('clas | prec | reca')
+    print('-----+------+-----')
+    for c in range(num_classes):
+        try: precision = class_accuracy[c][0]/(class_accuracy[c][0]+class_accuracy[c][1])
+        except: precision = 0
+        
+        try: recall = class_accuracy[c][0]/(class_accuracy[c][0]+class_accuracy[c][2])
+        except: recall = 0
+        
+        print(f'{c:>4} | {precision:.2f} | {recall:.2f}')
+
+def game_time(images, perc, targets):
+    print(f'shape of list images {np.shape(images)}')
+    total_samples = len(images)
+    for _ in range(20):
+        i = int(random.random()*total_samples)
+        plt.imshow(images[i].squeeze(), cmap="gray")
+        plt.show()
+        user_input = input(' user guess: ')
+        if user_input not in ['0','1','2','3','4','5','6','7']:
+            user_input = '0'
+        user_input = int(user_input)
+        model_guess = [f'   {emotions[j]} {"."*(round(perc[i][j]*20))}' for j in range(num_classes)]
+        model_guess[targets[i]] = ' D' + model_guess[targets[i]][2:]
+        model_guess[user_input] = 'U' + model_guess[user_input][1:]
+        model_guess = '\n'.join(model_guess)
+        print(            f' data label: {targets[i]}')
+        print(            f'model guess: \n{model_guess}')
+        plt.imshow(images[i].squeeze(), cmap="gray")
+        plt.show()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+batch_size = 32
 num_classes = 7
 
+transform = transforms.Compose([
+    transforms.Grayscale(),          # ensure 1 channel
+    transforms.Resize((48, 48)),     # FER standard size
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+val_dataset = datasets.ImageFolder("1/test", transform=transform)
+val_loader = DataLoader(val_dataset,  batch_size=batch_size)
+
+path_to_model = "models/" + os.listdir("models")[-1]
+print(f'testing model: {path_to_model}')
+model = EmotionCNN()
+model.load_state_dict(torch.load(path_to_model, map_location=device))
+model.to(device)
+model.eval()
+
+all_images   = []
+all_preds    = []
+all_percents = []
+all_classes  = []
+all_targets  = []
+with torch.no_grad():
+    for images, labels in val_loader:
+        images = images.to(device)
+        all_images.append(images)
+        logits = model(images)              # shape (N, C)
+        probs = torch.softmax(logits, dim=1)
+        preds = torch.argmax(logits, dim=1) # predicted class indices
+        percents = probs[ torch.arange(probs.size(0)), labels.to(device) ] # similar to preds, but instead of class indicies, it is percents of the target class
+        all_preds.append(preds.cpu())
+        all_percents.append(percents.cpu())
+        all_classes.append(probs.cpu())
+        all_targets.append(labels.cpu())
+
+all_images   = torch.cat(all_images  ).cpu().numpy()
 all_preds    = torch.cat(all_preds   ).cpu().numpy().ravel()
 all_percents = torch.cat(all_percents).cpu().numpy().ravel()
 all_classes  = torch.cat(all_classes ).cpu().numpy()
-print(f'all_classes shape: {np.shape(all_classes)}')
 all_targets  = torch.cat(all_targets ).cpu().numpy().ravel()
 
 train_dist = get_data_distribution('train')
 test_dist  = get_data_distribution('test')
 print(f'train distribution: {train_dist}')
 print(f'test distribution: {test_dist}')
+
+precision_recall(all_preds, all_targets)
 
 # choose which model quality analysis test to check
 match 3:
@@ -98,31 +157,7 @@ match 3:
         plt.xticks(range(num_classes))
         plt.yticks(range(num_classes))
         plt.show()
-    case 2: # total accuracy + precision/recall for each class
-        accuracy = 0
-        total_samples = 0
-        # TP, FP, FN
-        class_accuracy = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        for t, p in zip(all_targets, all_preds):
-            if t == p:
-                accuracy += 1
-                class_accuracy[t][0] += 1
-            else:
-                class_accuracy[p][1] += 1
-                class_accuracy[t][2] += 1
-            total_samples += 1
-        print(f'total accuracy: {(accuracy/total_samples):.2f}')
-        print('clas | prec | reca')
-        print('-----+------+-----')
-        for c in range(num_classes):
-            try: precision = class_accuracy[c][0]/(class_accuracy[c][0]+class_accuracy[c][1])
-            except: precision = 0
-            
-            try: recall = class_accuracy[c][0]/(class_accuracy[c][0]+class_accuracy[c][2])
-            except: recall = 0
-            
-            print(f'{c:>4} | {precision:.2f} | {recall:.2f}')
-    case 3: # confusion matrix using percentages of each class
+    case 2: # confusion matrix using percentages of each class
         cm = confusion_matrix_percent(all_classes, all_targets, num_classes=num_classes)
 
         plt.imshow(cm, interpolation='nearest', cmap='Blues')
@@ -133,4 +168,5 @@ match 3:
         plt.xticks(range(num_classes))
         plt.yticks(range(num_classes))
         plt.show()
-
+    case 3: # game time !
+        game_time(all_images, all_classes, all_targets)
